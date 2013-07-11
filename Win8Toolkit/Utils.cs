7175c428e3a6f58d872abd7e8470e48aebd0f3f5
unknown <hetappen@MININT-4LSLVP0.redmond.corp.microsoft.com>
2013-07-11 16:08:13 -0700

    Updating references

---
+++ Win8Toolkit/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Management.Automation;
+using Microsoft.Windows.Appx.PackageManager.Commands;
+
+namespace Win8Toolkit
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    class ConvertPackageToString : ArgumentTransformationAttribute
+    {
+        public override object Transform(EngineIntrinsics engineIntrinsics, object inputData)
+        {
+            if (inputData is PSObject)
+            {
+                var psInput = (PSObject)inputData;
+                if (psInput.BaseObject is AppxPackage)
+                {
+                    var appxInput = (AppxPackage)psInput.BaseObject;
+                    return appxInput.PackageFullName;
+                }
+            }
+
+            return inputData;
+        }
+    }
+}
