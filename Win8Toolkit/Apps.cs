12471a47fd26288b1d0a6d6253ae58a187e3de56
Henry Tappen <htappen@gmail.com>
2013-07-11 21:54:05 -0700

    Basic functionality to retrive apps

---
+++ Win8Toolkit/Apps.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Management.Automation;
+using System.Xml.Linq;
+using Windows.Management.Deployment;
+using System.Collections.Generic;
+using Windows.ApplicationModel;
+
+namespace Win8Toolkit
+{
+    [Cmdlet(VerbsCommon.Get, "AppsFromAppxPackage")]
+    public class AppEnumerate : PSCmdlet
+    {
+        private PackageManager pm = new PackageManager();
+        [Parameter(Mandatory = false, Position = 0, ValueFromPipeline = true)]
+        [ValidateNotNullOrEmpty]
+        [ConvertPackageToString]
+        public String PackageFullName { get; set; }
+ 
+        protected override void ProcessRecord()
+        {
+            IEnumerable<Package> packages;
+            if (this.PackageFullName == null)
+            {
+                packages = pm.FindPackagesForUser("");
+            }
+            else
+            {
+                packages = pm.FindPackagesForUser("", this.PackageFullName);
+            }
+            foreach (var package in packages)
+            {
+                String path = package.InstalledLocation.Path + @"\AppxManifest.xml";
+
+                XElement root = XDocument.Load(path).Root;
+                XNamespace xmlns = root.GetDefaultNamespace();
+                var q = from node in root.Descendants(xmlns + "Application") select node;
+                foreach (var node in q)
+                {
+                    XElement visualElements = node.Descendants(xmlns + "VisualElements").First();
+                    WriteObject(new WindowsStoreApplication(
+                        node.Attribute("Id").Value,
+                        package.Id.FullName,
+                        visualElements.Attribute("DisplayName").Value));
+                }
+            }
+        }
+    }
+
+    public class WindowsStoreApplication
+    {
+        public WindowsStoreApplication(
+            String packageName,
+            String name,
+            String displayName)
+        {
+            _packageName = packageName;
+            _name = name;
+            _displayName = displayName;
+        }
+
+        private String _packageName;
+        public String PackageName { get { return _packageName; } }
+        private String _name;
+        public String Name { get { return _name; } }
+        private String _displayName;
+        public String DisplayName { get { return _displayName; } }
+
+
+    }
+}
---
+++ Win8Toolkit/Win32.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Win8Toolkit
+{
+    [ComImport(), Guid("45BA127D-10A8-46EA-8AB7-56EA9078943C")]
+    class ApplicationActivationManager
+    {
+    }
+
+    enum ACTIVATEOPTIONS
+    {
+        AO_NONE           = 0x00000000,  // No flags set
+        AO_DESIGNMODE     = 0x00000001,  // The application is being activated for design mode, and thus will not be able to
+                                         // to create an immersive window. Window creation must be done by design tools which
+                                         // load the necessary components by communicating with a designer-specified service on
+                                         // the site chain established on the activation manager.  The splash screen normally
+                                         // shown when an application is activated will also not appear.  Most activations
+                                         // will not use this flag.
+        AO_NOERRORUI      = 0x00000002,  // Do not show an error dialog if the app fails to activate.                                
+        AO_NOSPLASHSCREEN = 0x00000004,  // Do not show the splash screen when activating the app.
+    }
+
+    [ComImport, Guid("2e941141-7f97-4756-ba1d-9decde894a3d"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    interface IApplicationActivationManager
+    {
+        // Activates the specified immersive application for the "Launch" contract, passing the provided arguments
+        // string into the application.  Callers can obtain the process Id of the application instance fulfilling this contract.
+        UInt32 ActivateApplication(
+            [In, MarshalAs(UnmanagedType.LPWStr)] String appUserModelId,
+            [In, MarshalAs(UnmanagedType.LPWStr)] String arguments,
+            [In] ACTIVATEOPTIONS options,
+            [Out] UInt32 processId);
+        UInt32 ActivateForFile(
+            [In, MarshalAs(UnmanagedType.LPWStr)] String appUserModelId,
+            [In] IntPtr itemArray, //[In] IShellItemArray itemArray,
+            [In, MarshalAs(UnmanagedType.LPWStr)] String verb,
+            [Out] UInt32 processId);
+        UInt32 ActivateForProtocol(
+            [In, MarshalAs(UnmanagedType.LPWStr)] String appUserModelId,
+            [In] IntPtr itemArray, //[In] IShellItemArray itemArray,
+            [Out] UInt32 processId);
+    }
+    /*
+    [ComImport, Guid ( "B63EA76D-1F85-456F-A19C-48159EFA858B" ), InterfaceType ( ComInterfaceType.InterfaceIsIUnknown )]
+    interface IShellItemArray
+    {
+        // Not supported: IBindCtx
+        [MethodImpl ( MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime )]
+        void BindToHandler ( [In, MarshalAs ( UnmanagedType.Interface )] IntPtr pbc, [In] ref Guid rbhid,
+                     [In] ref Guid riid, out IntPtr ppvOut );
+
+        [MethodImpl ( MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime )]
+        void GetPropertyStore ( [In] int Flags, [In] ref Guid riid, out IntPtr ppv );
+
+        [MethodImpl ( MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime )]
+        void GetPropertyDescriptionList ( [In] ref NativeMethods.PROPERTYKEY keyType, [In] ref Guid riid, out IntPtr ppv );
+
+        [MethodImpl ( MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime )]
+        void GetAttributes ( [In] NativeMethods.SIATTRIBFLAGS dwAttribFlags, [In] uint sfgaoMask, out uint psfgaoAttribs );
+
+        [MethodImpl ( MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime )]
+        void GetCount ( out uint pdwNumItems );
+
+        [MethodImpl ( MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime )]
+        void GetItemAt ( [In] uint dwIndex, [MarshalAs ( UnmanagedType.Interface )] out IShellItem ppsi );
+
+        // Not supported: IEnumShellItems (will use GetCount and GetItemAt instead)
+        [MethodImpl ( MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime )]
+        void EnumItems ( [MarshalAs ( UnmanagedType.Interface )] out IntPtr ppenumShellItems );
+    }*/
+
+    class Win32
+    {
+    }
+}
